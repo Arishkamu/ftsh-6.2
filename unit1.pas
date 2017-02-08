471da9e378c8cff371a1d0140c9cc09b30f97d87
Arina Ivanova <arishkame@gmail.com>
2017-02-08 19:45:30 +0300

    Paint домик

--- unit1.pas
+++ unit1.pas
@@ -5,13 +5,15 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button1: TButton;
     Image1: TImage;
     procedure Image1Click(Sender: TObject);
     procedure Image1MouseDown(Sender: TObject; Button: TMouseButton;
@@ -39,19 +41,44 @@ end;
 procedure TForm1.Image1MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-   Image1.Canvas.Rectangle(70, 100, 175, 200);
-   Image1.Canvas.Pen.Color:=clpurple;
-   Image1.Canvas.Brush.Color:=clblue;
-   Image1.Canvas.Pen.Width:=7;
-   Image1.Canvas.Rectangle(35, 50, 130, 150);
-   Image1.Canvas.Pen.Color:=clpurple;
-   Image1.Canvas.Brush.Color:=clblue;
-   Image1.Canvas.Pen.Width:=7;
-   Image1.Canvas.Rectangle(130, 150, 230, 250);
+   Image1.Canvas.Pen.Width:=1;
    Image1.Canvas.Pen.Color:=clgreen;
-   Image1.Canvas.Brush.Color:=clred;
-   Image1.Canvas.Pen.Width:=7;
+   Image1.Canvas.Brush.Color:=clgreen;
+   Image1.Canvas.Rectangle(0, 200, 400, 400);
 
+   Image1.Canvas.Pen.Width:=1;
+   Image1.Canvas.Pen.Color:=clskyblue;
+   Image1.Canvas.Brush.Color:=clskyblue;
+   Image1.Canvas.Rectangle(0, 0, 400, 200);
+
+   Image1.Canvas.Pen.Width:=1;
+   Image1.Canvas.Pen.Color:=clblack;
+   Image1.Canvas.Brush.Color:=clMedGray;
+   Image1.Canvas.Rectangle(150, 150, 250, 250);
+
+   Image1.Canvas.Pen.Width:=1;
+   Image1.Canvas.Pen.Color:=clblack;
+   Image1.Canvas.Brush.Color:=clblue;
+   Image1.Canvas.Rectangle(190, 190, 200, 200);
+   Image1.Canvas.Pen.Width:=1;
+   Image1.Canvas.Pen.Color:=clblack;
+   Image1.Canvas.Brush.Color:=clblue;
+   Image1.Canvas.Rectangle(200, 190, 210, 200);
+   Image1.Canvas.Pen.Width:=1;
+   Image1.Canvas.Pen.Color:=clblack;
+   Image1.Canvas.Brush.Color:=clblue;
+   Image1.Canvas.Rectangle(200, 200, 210, 210);
+   Image1.Canvas.Pen.Width:=1;
+   Image1.Canvas.Pen.Color:=clblack;
+   Image1.Canvas.Brush.Color:=clblue;
+   Image1.Canvas.Rectangle(190, 200, 200, 210);
+
+   Image1.Canvas.Pen.Width:=10;
+   Image1.Canvas.Pen.Color:=clred;
+   Image1.Canvas.MoveTo(135, 150);
+   Image1.Canvas.LineTo(265, 150);
+   Image1.Canvas.LineTo(200, 100);
+   Image1.Canvas.LineTo(135, 150);
 end;
 
 end.
