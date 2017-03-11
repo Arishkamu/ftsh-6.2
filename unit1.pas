12419b98d0956cd49f29abdd88fb61cd842ce655
Arina Ivanova <arishkame@gmail.com>
2017-03-11 14:29:32 +0300

    клик внутри квадрата и круга

--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  StdCtrls;
+  StdCtrls, Math;
 
 type
 
@@ -15,9 +15,16 @@ type
   TForm1 = class(TForm)
     Button1: TButton;
     Image1: TImage;
+    Timer1: TTimer;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
     procedure Image1Click(Sender: TObject);
     procedure Image1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
+    procedure Image1MouseEnter(Sender: TObject);
+    procedure Image1MouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure Timer1Timer(Sender: TObject);
   private
     { private declarations }
   public
@@ -26,6 +33,7 @@ type
 
 var
   Form1: TForm1;
+  x1, y1, x2, y2, r, xc, yc : integer;
 
 implementation
 
@@ -38,47 +46,76 @@ begin
 
 end;
 
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+   Image1.Canvas.Brush.Color:=clwhite;
+    Image1.Canvas.Rectangle(-10, -10, 1000, 1000);
+    Image1.Canvas.Pen.Color:=cllime;
+    Image1.Canvas.Pen.Width:=5;
+    Image1.Canvas.Brush.Color:=clblue;
+    Image1.Canvas.Rectangle(x1, y1, x2, y2);
+    Image1.Canvas.Ellipse(xc - r, yc - r, xc + r, yc + r);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+   x1:=50; x2:=150;
+   y1:=90; y2:=190;
+
+   xc:=200; yc:=150;
+   r:=35;
+  begin
+    Image1.Canvas.Brush.Color:=clwhite;
+    Image1.Canvas.Rectangle(-10, -10, 1000, 1000);
+    Image1.Canvas.Pen.Color:=cllime;
+    Image1.Canvas.Pen.Width:=5;
+    Image1.Canvas.Brush.Color:=clblue;
+    Image1.Canvas.Rectangle(x1, y1, x2, y2);
+    Image1.Canvas.Ellipse(xc - r, yc - r, xc + r, yc + r);
+  end;
+end;
+
 procedure TForm1.Image1MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-   Image1.Canvas.Pen.Width:=1;
-   Image1.Canvas.Pen.Color:=clgreen;
-   Image1.Canvas.Brush.Color:=clgreen;
-   Image1.Canvas.Rectangle(0, 200, 400, 400);
 
-   Image1.Canvas.Pen.Width:=1;
-   Image1.Canvas.Pen.Color:=clskyblue;
-   Image1.Canvas.Brush.Color:=clskyblue;
-   Image1.Canvas.Rectangle(0, 0, 400, 200);
+end;
 
-   Image1.Canvas.Pen.Width:=1;
-   Image1.Canvas.Pen.Color:=clblack;
-   Image1.Canvas.Brush.Color:=clMedGray;
-   Image1.Canvas.Rectangle(150, 150, 250, 250);
+procedure TForm1.Image1MouseEnter(Sender: TObject);
+begin
 
-   Image1.Canvas.Pen.Width:=1;
-   Image1.Canvas.Pen.Color:=clblack;
-   Image1.Canvas.Brush.Color:=clblue;
-   Image1.Canvas.Rectangle(190, 190, 200, 200);
-   Image1.Canvas.Pen.Width:=1;
-   Image1.Canvas.Pen.Color:=clblack;
-   Image1.Canvas.Brush.Color:=clblue;
-   Image1.Canvas.Rectangle(200, 190, 210, 200);
-   Image1.Canvas.Pen.Width:=1;
-   Image1.Canvas.Pen.Color:=clblack;
-   Image1.Canvas.Brush.Color:=clblue;
-   Image1.Canvas.Rectangle(200, 200, 210, 210);
-   Image1.Canvas.Pen.Width:=1;
-   Image1.Canvas.Pen.Color:=clblack;
-   Image1.Canvas.Brush.Color:=clblue;
-   Image1.Canvas.Rectangle(190, 200, 200, 210);
+end;
 
-   Image1.Canvas.Pen.Width:=10;
-   Image1.Canvas.Pen.Color:=clred;
-   Image1.Canvas.MoveTo(135, 150);
-   Image1.Canvas.LineTo(265, 150);
-   Image1.Canvas.LineTo(200, 100);
-   Image1.Canvas.LineTo(135, 150);
+procedure TForm1.Image1MouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+   if (x<=x2) and (x>=x1) and (y>=y1) and (y<=y2) then
+    begin
+      Image1.Canvas.Brush.Color:=clwhite;
+      Image1.Canvas.Rectangle(-10, -10, 1000, 1000);
+      Image1.Canvas.Pen.Color:=clyellow;
+      Image1.Canvas.Pen.Width:=5;
+      Image1.Canvas.Brush.Color:=cllime;
+      Image1.Canvas.Rectangle(x1, y1, x2, y2);
+    end;
+
+    if sqrt((x-xc) * (x-xc) + (y-yc) * (y-yc)) <= r Then
+     begin
+       Image1.Canvas.Brush.Color:=clwhite;
+       Image1.Canvas.Rectangle(-10, -10, 1000, 1000);
+       Image1.Canvas.Pen.Color:=clyellow;
+       Image1.Canvas.Pen.Width:=5;
+       Image1.Canvas.Brush.Color:=cllime;
+       Image1.Canvas.Ellipse(xc - r, yc - r, xc + r, yc + r);
+     end;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+
+Begin
+
+end;
 end;
 
 end.
